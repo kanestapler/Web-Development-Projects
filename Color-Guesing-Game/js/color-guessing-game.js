4b82592ac18baaf30b054caa42df5815de48023c
Luis Gamarra Jimenez <lgamarra26@icloud.com>
2016-05-26 18:35:03 -0400

    Added functionality to easy and hard buttons. Also created random color generator

--- Color-Guesing-Game/js/color-guessing-game.js
+++ Color-Guesing-Game/js/color-guessing-game.js
@@ -1,20 +1,59 @@
-var colors = [
-  "rgb(250, 0, 0)",
-  "rgb(0, 250, 0)",
-  "rgb(250, 0, 250)",
-  "rgb(0, 250, 250)",
-  "rgb(0, 0, 250)",
-  "rgb(250, 250, 0)"
-]
+var colors = generateRandomColors();
 
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.querySelector("#color-display");
 var colorToBeGuessed = colors[0];
+var easyButton = document.querySelector("#easy-mode-button");
+var hardButton = document.querySelector("#hard-mode-button");
+var hardModeSquares = document.querySelectorAll(".hard-mode-squares")
 var text = colorToBeGuessed.toUpperCase();
+var easyModeToggle;
+
 colorDisplay.textContent = text;
+
+console.log(easyButton);
+
+easyButton.addEventListener("click", function () {
+
+  for (var i = 0; i < hardModeSquares.length; i++) {
+    hardModeSquares[i].style.display = "none";
+  }
+  easyModeToggle = true;
+});
+
+hardButton.addEventListener("click", function () {
+
+  if (easyModeToggle === true) {
+    for (var i = 0; i < hardModeSquares.length; i++) {
+      hardModeSquares[i].style.display = "block";
+    }
+  }
+  easyModeToggle = false;
+});
+
 
 
 // assigns the color to each square
 for (var i = 0; i < 6; i++) {
   squares[i].style.background = colors[i];
+}
+
+function generateRandomColors() {
+  var tempColorArr = [];
+
+  for (var i = 0; i < 6; i++) {
+    var color = generateColor();
+    tempColorArr[i] = color;
+  }
+
+  return tempColorArr;
+}
+
+function generateColor() {
+
+  var r = Math.floor(Math.random() * 251);
+  var g = Math.floor(Math.random() * 251);
+  var b = Math.floor(Math.random() * 251);
+
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
